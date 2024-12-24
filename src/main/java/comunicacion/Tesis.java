6a19017569186f0a83e7286b290716a81e9853ca
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-24 09:28:43 -0500

    Create Tesis.java

---
+++ src/main/java/comunicacion/Tesis.java
@@ -0,0 +1,9 @@
+package comunicacion;
+
+public class Tesis extends Escrito{
+    private String idea;
+    private String[] argumentos;
+    private String conclusion;
+    private String referencias;
+    private String interpretacion;
+}
