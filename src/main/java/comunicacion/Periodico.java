a97f09e916ed9f414260826288e27c5f9e959452
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-24 09:52:40 -0500

    Update Periodico.java

--- src/main/java/comunicacion/Periodico.java
+++ src/main/java/comunicacion/Periodico.java
@@ -4,4 +4,11 @@ public class Periodico extends Escrito{
     private String fecha;
     private String primicia;
     private String interpretacion;
+
+    public Periodico(String origen, String titulo, String autor, int paginas, String fecha, String primicia, String interpretacion){
+        super(origen, titulo, autor, paginas);
+        this.fecha = fecha;
+        this.primicia = primicia;
+        this.interpretacion = interpretacion;
+    }
 }
