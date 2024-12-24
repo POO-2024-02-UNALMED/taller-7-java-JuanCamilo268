992a261ae17ba16281b6fd50eca8a2b271207655
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-24 09:52:45 -0500

    Update Fabula.java

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -2,5 +2,11 @@ package comunicacion;
 
 public class Fabula extends Escrito{
     private String ensenanzas;
-    private String Interpretacion;
+    private String interpretacion;
+
+    public Fabula(String origen, String titulo, String autor, int paginas, String ensenanzas, String interpretacion){
+        super(origen, titulo, autor, paginas);
+        this.ensenanzas = ensenanzas;
+        this.interpretacion = interpretacion;
+    }
 }
