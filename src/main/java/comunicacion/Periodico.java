427f6eed654400a5f8504fb42dde012764e8a7f1
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-24 10:12:03 -0500

    Update Periodico.java

--- src/main/java/comunicacion/Periodico.java
+++ src/main/java/comunicacion/Periodico.java
@@ -11,4 +11,12 @@ public class Periodico extends Escrito{
         this.primicia = primicia;
         this.interpretacion = interpretacion;
     }
+
+    public void setInterpretacion(String interpretacion){this.interpretacion = interpretacion;}
+    public void setFecha(String fecha){this.fecha = fecha;}
+    public void setPrimicia(String primicia){this.primicia = primicia;}
+
+    public String getInterpretacion(){return interpretacion;}
+    public String getFecha(){return fecha;}
+    public String getPrimicia(){return primicia;}
 }
