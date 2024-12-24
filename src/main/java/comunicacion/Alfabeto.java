656d8b88bbdc22bbb217731722c112404226da3c
JuanCamilo268 <juleonba@unal.edu.co>
2024-12-24 09:52:48 -0500

    Update Alfabeto.java

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -3,5 +3,11 @@ package comunicacion;
 public class Alfabeto extends Pictograma{
     private String[] letras;
     private String interpretacion;
-
+    public Alfabeto (String origen, String[] letras, String interpretacion){
+        super(origen);
+        this.letras = letras;
+        this.interpretacion = interpretacion;
+    }
+    public void setLetras(String[] letras){this.letras = letras;}
+    public void setInterpretacion(String interpretacion){this.interpretacion = interpretacion;}
 }
